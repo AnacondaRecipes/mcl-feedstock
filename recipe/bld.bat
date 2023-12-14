bfa986283da68c4aefd85296ccab32e98eac0e92
Marek Waszkiewicz <mwaszkiewicz@anaconda.com>
2023-12-15 00:22:34 +0100

    try to build on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,12 +3,17 @@ cd build
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake -GNinja ^
-    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^ 
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DMCL_STATIC_LIB=OFF ^
     ..
+
 if %ERRORLEVEL% neq 0 exit 1
 
-cmake --build .
+ninja -v -w dupbuild=warn
+:: cmake --build . -- -w dupbuild=warn
 if %ERRORLEVEL% neq 0 exit 1
 
-cmake --build . --target install
+
+ninja -v -w dupbuild=warn install
+:: cmake --build . -- -w dupbuild=warn --target install
 if %ERRORLEVEL% neq 0 exit 1
